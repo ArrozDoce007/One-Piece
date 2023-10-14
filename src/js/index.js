5a2426fa2663ab27e28ff7c27e8bf59b172f4140
ArrozDoce007 <143344186+ArrozDoce007@users.noreply.github.com>
2023-10-14 15:00:38 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+let audioAtual;
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+const nomesAudios = ["mar sunny.mp3", "chopper.mp3", "zoro.mp3", "luffy.mp3", "nami.mp3", "sanji.mp3"];
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+
+        if (audioAtual) {
+            audioAtual.pause();
+            audioAtual.currentTime = 0;
+        }
+
+        tocarSom(nomesAudios[indice]);
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
+function tocarSom(nomeAudio) {
+    audioAtual = new Audio(`./src/audios/${nomeAudio}`);
+    audioAtual.play();
+}
